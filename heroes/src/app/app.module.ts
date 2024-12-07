a0d70c8ccca95211717dd646aeeb0cb4318961d0
jesus <frontendjesus@gmail.com>
2024-12-07 11:04:20 +0100

    Concepto de Modulos.

--- heroes/src/app/app.module.ts
+++ heroes/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
+import { HeroComponent } from './heroes/hero/components/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
-import { contadorComponent } from './contador/components/contador/contador.component';
+import { ContadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule, AppRoutingModule, contadorComponent ],
+  declarations: [AppComponent, ListComponent],
+  imports: [BrowserModule, AppRoutingModule, ContadorModule, HeroesModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ heroes/src/app/heroes/heroes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HeroComponent } from './hero/components/hero/hero.component';
+
+@NgModule({
+    imports: [],
+    exports: [HeroComponent],
+    declarations: [HeroComponent],
+    providers: [],
+})
+export class HeroesModule { }
